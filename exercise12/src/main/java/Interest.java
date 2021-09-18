d47a2c39058914eeddb0594beec204cf455d0b35
Brian <Brian.reed1202@gmail.com>
2021-09-18 19:15:59 -0400

    Added exercise12

---
+++ exercise12/src/main/java/Interest.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Brian Reed
+ */
+import java.util.Scanner;
+class Interest {
+    double principal;
+    double rateOfInterest;
+    int numYears;
+    public void read() {
+        //prompts user for relevant data (principal, roi, years)
+        //Stores data
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the principal: ");
+        this.principal = sc.nextDouble();
+        System.out.print("Enter the rate of interest: ");
+        this.rateOfInterest = sc.nextDouble();
+        System.out.print("Enter the number of years: ");
+        this.numYears = sc.nextInt();
+    }
+    public void print() {
+        //calculates A using A = P(1 + rt) and rounds to the nearest $0.01
+        //print relevant data ("After x years at y% the investment will be worth $z")
+        int amount = (int) Math.round(principal * (1 + rateOfInterest * numYears * 0.01));
+        System.out.println("After " + numYears + " years at " + rateOfInterest + "%, the investment will be worth $" + amount + ".");
+    }
+}
---
+++ exercise12/src/main/java/Solution12.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Brian Reed
+ */
+public class Solution12{
+    public static void main(String[] args) {
+        //make object "Interest"
+        //call read method to take input
+        //call print method to output
+        Interest call = new Interest();
+        call.read();
+        call.print();
+    }
+}
